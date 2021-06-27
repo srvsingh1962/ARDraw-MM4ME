c5f231370fd5c024bd3b21a06fad8e72c38bb724
sourav singh <srvsingh1962@gmail.com>
2021-06-27 11:27:53 +0530

    AR Draw Project files

---
+++ ARDraw/Assets/Scripts/ARDrawManager.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(ARAnchorManager))]
+public class ARDrawManager : Singleton<ARDrawManager>
+{
+    [SerializeField]
+    private LineSettings lineSettings = null;
+
+    [SerializeField]
+    TMP_Dropdown _linewidthdropdown;
+
+    [SerializeField]
+    GameObject _canvas;
+
+    [SerializeField]
+    private UnityEvent OnDraw = null;
+
+    [SerializeField]
+    private ARAnchorManager anchorManager = null;
+
+    [SerializeField]
+    private Camera arCamera = null;
+
+    public static event Action Ontouched;
+
+    private List<ARAnchor> anchors = new List<ARAnchor>();
+
+    private Dictionary<int, ARLine> Lines = new Dictionary<int, ARLine>();
+
+    private bool CanDraw { get; set; }
+
+    float[] linewidth = { 0.005f, 0.01f, 0.015f, 0.02f, 0.025f, 0.03f};
+
+    void Update()
+    {
+        _canvas.SetActive(CanDraw);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) && CanDraw)
+        {
+            return;
+        }
+#if !UNITY_EDITOR
+        DrawOnTouch();
+#else
+        DrawOnMouse();
+#endif
+    }
+
+    public void AllowDraw(bool isAllow)
+    {
+        CanDraw = isAllow;
+    }
+
+    public void ChangeLineWidth()
+    {
+        lineSettings.startWidth = linewidth[_linewidthdropdown.value];
+        lineSettings.endWidth = linewidth[_linewidthdropdown.value];
+    }
+
+    void DrawOnTouch()
+    {
+        if (!CanDraw) return;
+
+        int tapCount = Input.touchCount > 1 && lineSettings.allowMultiTouch ? Input.touchCount : 1;
+
+        for (int i = 0; i < tapCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            Vector3 touchPosition = arCamera.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, lineSettings.distanceFromCamera));
+
+            //ARDebugManager.Instance.LogInfo($"{touch.fingerId}");
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                OnDraw?.Invoke();
+                Ontouched?.Invoke();
+
+#pragma warning disable CS0618 // Type or member is obsolete
+                ARAnchor anchor = anchorManager.AddAnchor(new Pose(touchPosition, Quaternion.identity));
+#pragma warning restore CS0618 // Type or member is obsolete
+                if (anchor == null)
+                    Debug.LogError("Error creating reference point");
+                else
+                {
+                    anchors.Add(anchor);
+                    //ARDebugManager.Instance.LogInfo($"Anchor created & total of {anchors.Count} anchor(s)");
+                }
+
+                ARLine line = new ARLine(lineSettings);
+                Lines.Add(touch.fingerId, line);
+                line.AddNewLineRenderer(transform, anchor, touchPosition);
+            }
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                Lines[touch.fingerId].AddPoint(touchPosition);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                Lines.Remove(touch.fingerId);
+            }
+        }
+    }
+
+    void DrawOnMouse()
+    {
+        if (!CanDraw) return;
+
+        Vector3 mousePosition = arCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, lineSettings.distanceFromCamera));
+
+        if (Input.GetMouseButton(0))
+        {
+            OnDraw?.Invoke();
+
+            if (Lines.Keys.Count == 0)
+            {
+                ARLine line = new ARLine(lineSettings);
+                Lines.Add(0, line);
+                line.AddNewLineRenderer(transform, null, mousePosition);
+            }
+            else
+            {
+                Lines[0].AddPoint(mousePosition);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Lines.Remove(0);
+        }
+    }
+
+    GameObject[] GetAllLinesInScene()
+    {
+        return GameObject.FindGameObjectsWithTag("Line");
+    }
+
+    public void ClearLines()
+    {
+        GameObject[] lines = GetAllLinesInScene();
+        foreach (GameObject currentLine in lines)
+        {
+            LineRenderer line = currentLine.GetComponent<LineRenderer>();
+            Destroy(currentLine);
+        }
+    }
+}
